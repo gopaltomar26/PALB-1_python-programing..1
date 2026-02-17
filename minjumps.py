eb52eb955df5f04106b7f2f124aef5cddecc78a2
gopal tomar <gopal2006tomar@gmail.com>
2026-02-18 02:54:09 +0530

    Create minjumps.py

---
+++ minjumps.py
@@ -0,0 +1,21 @@
+class Solution:
+	def minJumps(self, arr):
+    n=len(arr)
+    if n<=0:
+      return 0
+    if arr[0]==0:
+      return -1
+    maxReach=arr[0]
+    step=arr[0]
+    jump=1
+    for i in range(1,n):
+      if i ==n-1:
+        return jump
+    maxReach=max(maxReach,arr[i]+i)
+    step-=1
+    if step==0:
+      jump+=1
+      if i>=maxReach:
+        return -1
+      step=maxReach-i
+  return -1
